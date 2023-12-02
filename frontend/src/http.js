873df660d374e1b8b9a7d293be695da7806ac723
Suresh Roshi <sureshroshi007@gmail.com>
2023-12-03 01:36:48 +0530

    added Dockerfile

--- frontend/src/http.js
+++ frontend/src/http.js
@@ -1,5 +1,5 @@
 export async function fetchAvailablePlaces() {
-  const response = await fetch("http://localhost:3000/places");
+  const response = await fetch("http://localhost:8080/places");
   const resData = await response.json();
 
   if (!response.ok) {
@@ -9,7 +9,7 @@ export async function fetchAvailablePlaces() {
 }
 
 export async function updateUserPlaces(places) {
-  const response = await fetch("http://localhost:3000/user-places", {
+  const response = await fetch("http://localhost:8080/user-places", {
     method: "PUT",
     body: JSON.stringify({ places }),
     headers: {
@@ -24,14 +24,11 @@ export async function updateUserPlaces(places) {
 }
 
 export async function fetchUserPlaces() {
-  const response = await fetch("http://localhost:3000/user-places");
+  const response = await fetch("http://localhost:8080/user-places");
   const resData = await response.json();
 
   if (!response.ok) {
     throw new Error("Failed to fetch user places");
   }
   return resData.places;
-
 }
-
-
